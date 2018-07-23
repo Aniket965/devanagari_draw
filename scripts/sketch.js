b65f53f5771c3c6c5be87acd4369d77129dc2a07
aniket965 <aniket965.as@gmail.com>
2018-07-23 22:19:31 +0530

    Extracted Pixels from canvas

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,3 +1,5 @@
+let painting = false 
+
 function setup() {
     var div = document.getElementById('sketchdiv')
     var canvas = createCanvas(320, 320)
@@ -6,5 +8,36 @@ function setup() {
 }
 
 function draw() {
+    strokeWeight(8);
+    stroke(255);
+    
+    if (mouseIsPressed && painting) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
 
 }
+
+function mousePressed() {
+    painting = true;
+
+  }
+  // Stop
+  function mouseReleased() {
+    painting = false;
+  }
+  
+document.addEventListener('DOMContentLoaded', _ => {
+    let predict = document.getElementById('predict')
+    predict.addEventListener('click', _ => {
+        let input_pixels = [];
+        let img = get()
+        img.resize(32, 32)
+        img.loadPixels()
+        for (let i = 0; i < 1024; i++) {
+            let grayscale = img.pixels[i * 4];
+            input_pixels[i] = grayscale;
+        }
+        console.log(input_pixels)
+    })
+
+}, false);
